ea11d9912ff7793336cc9946cf5d8a87bb03976c
marwabounaas2 <marwabounaas05@gmail.com>
2025-05-20 22:24:30 +0200

    premier

---
+++ src/Component/Fonctoin4.jsx
@@ -0,0 +1,7 @@
+function Fonctoin4()
+{
+return(
+<h1>bounjour sara</h1>
+)
+}
+export default Fonctoin4
--- src/Component/Fonctoin5.jsx
+++
@@ -1,7 +0,0 @@
-function Fonctoin5()
-{
-return(
-    <h1>bounjour sara</h1>
-)
-}
-export default Fonctoin5
