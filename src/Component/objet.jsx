53e3bd23733c2a7216fd40a991ae974b5b38afa7
marwabounaas2 <marwabounaas05@gmail.com>
2025-05-20 10:48:58 +0200

    Update objet.jsx

--- src/Component/objet.jsx
+++ src/Component/objet.jsx
@@ -4,6 +4,7 @@ return(
     <div>
         <h2 style={NomStyle}>designatoin Produit:{informatoin.nom}</h2>
         <h2>prix du Produit:{informatoin.prix}</h2>
+        <h3>bounjour mes fleur</h3>
     </div>
 )
 }
