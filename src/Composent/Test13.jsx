dea3b14921e61177eaadd602de6708fe0e32df20
marwabounaas2 <marwabounaas05@gmail.com>
2025-05-27 09:43:24 +0200

    test133

---
+++ src/Composent/Test13.jsx
@@ -0,0 +1,9 @@
+function Test13()
+{
+    return(
+        <div>
+
+        </div>
+    )
+}
+export default Test13
--- src/Composent/Test14.jsx
+++
@@ -1,8 +0,0 @@
-function Test14()
-{
-    return(
-<h1>beinvenu</h1>
-        <h2>sarra</h2>
-    )
-}
-export default Test14
